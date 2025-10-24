353086a8e503bde0a7da46413ece67c659fcbd82
Jia <117244684+jiancpp@users.noreply.github.com>
2025-10-25 01:50:20 +0800

    Add constraints for events core table

--- schema/constraints.sql
+++ schema/constraints.sql
@@ -4,23 +4,66 @@
 
 USE dbApp;
 
+-- ==========================================================
+--   EVENTS CORE AND SUBTABLES
+-- ==========================================================
+
 --
--- Constraints for `events`
+-- Constraints for `Events`
 --
-ALTER TABLE events
-ADD CONSTRAINT fk_events_artist
-    FOREIGN KEY (`artist_id`) REFERENCES artists(`id`),
-    ON DELETE CASCADE ON UPDATE CASCADE
-ADD CONSTRAINT fk_events_fanclub
-    FOREIGN KEY (`fanclub_id`) REFERENCES fanclubs(`id`),
-    ON DELETE SET NULL ON UPDATE CASCADE
+ALTER TABLE Events
+ADD CONSTRAINT fk_events_artists
+    FOREIGN KEY (`Artist_ID`) REFERENCES Artists(`Artist_ID`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT fk_events_fanclubss
+    FOREIGN KEY (`Fanclub_ID`) REFERENCES Fanclubs(`Fanclub_ID`)
+    ON DELETE SET NULL ON UPDATE CASCADE,
 ADD CONSTRAINT fk_events_venue
-    FOREIGN KEY (`venue_id`) REFERENCES venues(`id`),
-    ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (`Venue_ID`) REFERENCES Venues(`Venue_ID`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT chk_event_host_xor
 CHECK (
     (artist_id IS NOT NULL AND fanclub_id IS NULL)
  OR (artist_id IS NULL AND fanclub_id IS NOT NULL)
 );
 
+-- 
+-- Constraints for `Sections`
+-- 
+ALTER TABLE Sections
+ADD CONSTRAINT fk_sections_venues
+    FOREIGN KEY (`Venue_ID`) REFERENCES Venues(`Venue_ID`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
+-- 
+-- Constraints for `Seats`
+-- 
+ALTER TABLE Seats
+ADD CONSTRAINT fk_seats_venues
+    FOREIGN KEY (`Venue_ID`) REFERENCES Venues(`Venue_ID`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- 
+-- Constraints for `Ticket_Tier`
+-- 
+ALTER TABLE Ticket_Tiers
+ADD CONSTRAINT fk_tiers_events
+    FOREIGN KEY (`Event_ID`) REFERENCES Events(`Event_ID`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- 
+-- Constraints for `Ticket_Purchases`
+-- 
+ALTER TABLE Ticket_Purchases
+ADD CONSTRAINT fk_tickets_users
+    FOREIGN KEY (`User_ID`) REFERENCES Users(`User_ID`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT fk_tickets_events
+    FOREIGN KEY (`Event_ID`) REFERENCES Events(`Event_ID`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT fk_tickets_tiers
+    FOREIGN KEY (`Tier_ID`) REFERENCES Ticket_Tiers(`Tier_ID`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT fk_tickets_seats
+    FOREIGN KEY (`Seat_ID`) REFERENCES Seats(`Seats_ID`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
