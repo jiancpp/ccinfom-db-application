758b5fee3da5857b44f0736b201b39b6459e02ab
jiancpp <117244684+jiancpp@users.noreply.github.com>
2025-11-05 12:38:16 +0800

    Ignore config.py

--- app/config.py
+++
@@ -1,9 +0,0 @@
-# ============================================
-#           DATABASE CONFIGURATION
-# ============================================
-
-# Change according to you MySQL Workbench Credentials
-# Don't commit once done setting ups
-DB_USER = "root"
-DB_PASS = "Weloveinfom143!"
-DB_NAME = "dbApp"
