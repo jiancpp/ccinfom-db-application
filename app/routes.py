dc87c16ed8717c6e4a9435cf427753fe6470966f
jesmaeca <jezzisbored@gmail.com>
2025-11-19 01:06:52 +0800

    Edited home page

--- app/routes.py
+++ app/routes.py
@@ -23,9 +23,13 @@ def index():
             title = "Artists You Follow"
             fan_ID = g.current_user['Fan_ID'] if g.current_user else None 
             artist_query = '''
-                SELECT * FROM Artist AS a
+                SELECT a.*, 
+                       COUNT(af_count.Fan_ID) AS Num_Followers
+                FROM Artist AS a
                 JOIN Artist_Follower AS af ON a.Artist_ID = af.Artist_ID
+                LEFT JOIN Artist_Follower AS af_count ON a.Artist_ID = af_count.Artist_ID 
                 WHERE af.Fan_ID = %s
+                GROUP BY a.Artist_ID, a.Artist_Name
                 ORDER BY a.Artist_Name ASC;
                 '''
             total_artists_query = '''
@@ -895,8 +899,30 @@ def manage_fanclubs():
 
 @main_routes.route('/manage_artists')
 def manage_artists():
-    # edit
-    return render_template('manager_portal.html')
+    query = """
+        SELECT
+            A.Artist_ID,
+            A.Artist_Name,
+            A.Activity_Status,
+            A.Debut_Date,
+            M.Manager_Name,
+            M.Agency AS Manager_Agency,
+            COUNT(Mem.Member_ID) AS Member_Count
+        FROM
+            Artist A
+        LEFT JOIN
+            Manager M ON A.Manager_ID = M.Manager_ID
+        LEFT JOIN
+            Member Mem ON A.Artist_ID = Mem.Artist_ID
+        GROUP BY
+            A.Artist_ID, A.Artist_Name, A.Activity_Status, A.Debut_Date, M.Manager_Name, M.Agency
+        ORDER BY 
+            A.Artist_Name;
+        """
+        
+    artists = execute_select_query(query)
+        
+    return render_template('manage_artists.html', artists=artists)
 
 
 @main_routes.route('/manage_events')
